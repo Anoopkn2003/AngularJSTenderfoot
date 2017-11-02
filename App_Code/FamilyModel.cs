a7b6bf194be03815accb005a905717f319dd96ea
Anoop Nair Keerthiveetil <32155831+Anoopkn2003@users.noreply.github.com>
2017-11-02 15:26:06 +0200

    Add files via upload
    DBML file to connect with DB
    Change in routeconfig to all routing to function using XMLHTTPREQUEST

---
+++ App_Code/FamilyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FamilyModel
+/// </summary>
+public class FamilyModel
+{
+    public string Name;
+    public string City;
+    public int Age;
+
+    public FamilyModel()
+    {
+    }
+}
